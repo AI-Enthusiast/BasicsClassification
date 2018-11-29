ff3079f31fd59a4f6e9b37461c88e4e06dd88202
Cormac <builderofthefuture@gmail.com>
2018-11-28 23:47:19 -0500

    1.b Initial application of C's knowledge lots of math

--- P2.py
+++ P2.py
@@ -3,16 +3,16 @@
 # Excersise 1
 
 import csv
+import math
+import random
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from keras.layers import Dense, Activation
-from keras.models import Sequential, Model
 from sklearn.model_selection import train_test_split
 
-import math
+
 # Used to self report errors
-def error(errorMessage):
+def err(errorMessage):
     print("> ERROR:\t" + errorMessage)
 
 
@@ -34,7 +34,7 @@ def readFile(file):
 
 # Problem 1 of the homework
 def prob1():
-    data = pd.read_csv("irisdata.csv") #
+    data = pd.read_csv("irisdata.csv")  #
 
     # reassigns labels
     data.loc[data['species'] == "setosa", "species"] = 0
@@ -55,23 +55,52 @@ def prob1():
         plt.show()  # ggez
 
     def b():
+        print("temp")
+
         def predict(p):
             return 1 if p >= .5 else 0
 
-        def logistic(z):
-            return ((1/(1 + math.exp(-z))), math.exp(z) + math.exp(-z))
+        def sigmoid(z):
+            return 1 / (1 + math.exp(-z))
 
-        print(logistic(0))
+        def sigmoidDeriv(sigmoid):
+            try:
+                return sigmoid * (1 - sigmoid)
+            except RuntimeWarning as e:
+                print(sigmoid)
+                err(e)
+
+        def ErResWeiDerive(T, t, sigmoid, data):  # derivative of the error with respect to the weight
+            return (T + t) * sigmoidDeriv(sigmoid) * data
+
+        def error(T, t):
+            return .5 * ((T - t) ** 2)
+
         x = list(data.iloc[:, [2, 3]].values)  # if all else fails convert it to a string and remove the shit TEMP
         y = list(data.iloc[:, 4].values)
         # slit into testing and training
-        # x_train, x_test = (x[50:90,100:140]), (x[90:100],[140:150])
-        # y_train, y_test = y[50:90].append(y[100:140]), y[90:100].append(y[140:150])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1)
+        w1, w2, bias = random.random(), random.random(), -random.random()
 
+        for index in range(len(x_train)):
+            N = x_train[index][0] * w1 + x_train[index][0] * w2 + bias
+            T = sigmoid(N)
+            t = y_train[index]
+            print(T, predict(T))
+            w1 = w1 - error(T, t) * .1
+            w2 = w2 - error(T, t) * .1
+            try:
+                bias = bias - (T + t) * sigmoidDeriv(N) * .1
+            except RuntimeWarning as e:
+                print(N)
+                err(e)
+
+        print(" y = " + str(-w1 / w2) + " * x" + " - " + str(bias))
 
     # a()  # run part A
     b()  # run part B
 
 
 if __name__ == "__main__":
+    print("temp")
     prob1()  # run problem 1
