c24953d28206d3d9f039888ca30507a1d43ba71b
Cormac <builderofthefuture@gmail.com>
2018-12-01 16:32:19 -0500

    Pushed to work on laptop

--- P2.py
+++ P2.py
@@ -74,25 +74,39 @@ def prob1():
         def error(T, t):
             return .5 * ((T - t) ** 2)
 
-        x = list(data.iloc[:, [2, 3]].values)  # if all else fails convert it to a string and remove the shit TEMP
-        y = list(data.iloc[:, 4].values)
+        x = list(data.iloc[50:, [2, 3]].values)  # if all else fails convert it to a string and remove the shit TEMP
+        y = list(data.iloc[50:, 4].values)
         # slit into testing and training
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1)
         w1, w2, bias = random.random(), random.random(), -random.random()
+        # w1, w2 = 0,0 #actual weights
+        totErr = 0  # the total error
         for index in range(len(x_train)):
-            N = x_train[index][0] * w1 + x_train[index][0] * w2 + bias
+            N = x_train[index][0] * w1 + x_train[index][1] * w2 + bias
             T = sigmoid(N)
-            t = y_train[index]
-            print(T, predict(T))
-            w1 = w1 - error(T, t) * .1
-            w2 = w2 - error(T, t) * .1
-            try:
-                bias = bias - (T + t) * sigmoidDeriv(N) * .1
-            except RuntimeWarning as e:
-                print(N)
-                err(e)
+            t = (y_train[index] - 1)  # minus one bc 2nd and 3rd classes = 1 & 2
+            totErr += error(T, t)
+            print("(x,y)=", (x_train[index], t))
+            print("T,p(T),t = ", T, predict(T), t)  # Compair sigmoids result to predicted result to actual result
+            w1 = w1 - error(T, t) * ErResWeiDerive(T, t, T, w1)  # update w1
+            w2 = w2 - error(T, t) * ErResWeiDerive(T, t, T, w2)  # update w2
+            bias = bias - (T + t) * sigmoidDeriv(N) * ErResWeiDerive(T, t, T, w2)  # update bias
+        numWrong = 0
+        for index in range(len(x_test)):
+            if predict(sigmoid(x_test[index][0] * w1 + x_test[index][1] * w2 + bias)) != (y_test[index] - 1):
+                numWrong += 1
+        print(" y = " + str(-w1) + "/" + str(w2) + " * x" + " - " + str(bias))
+        print("errorRate =", str(100 * numWrong / (len(x_test) + len(y_test))) + "%")
+        print(bias)
 
-        print(" y = " + str(-w1 / w2) + " * x" + " - " + str(bias))
+    def c():
+        pass
+
+    def d():
+        pass
+
+    def e():
+        pass
 
     # a()  # run part A
     b()  # run part B
